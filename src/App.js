d239f0abc8c9e222effbf28b89eb5538d45c3790
vinodsubbanna <vinod.msg@gmail.com>
2019-10-29 13:07:37 -0500

    Roofstock Intial push with all the Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ListProperty from './components/ListProperty'
+import store from './server/store';
+import { Container, Row, Col } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridOrList: 'grid'
+    }
+  }
+
+  handleClick = (gridOrList) => {
+    this.setState(prevState => ({
+      gridOrList: gridOrList
+    }));
+  }
+
+  ListorGridViewByFlag = () => {
+    const isGrid = this.state.gridOrList === 'grid' ? true: false;
+    return isGrid ? 
+      <Col className="property-grid">
+        {<ListProperty gridOrList={this.state.gridOrList}/>}
+      </Col> : 
+      <Col md={12}>
+        <table className="property-list"> 
+        <thead>
+            <tr>
+                <th>image</th>
+                <th>Address</th>
+                <th>Year Built</th>
+                <th>List Price</th>
+                <th>Monthly Rent</th>
+                <th>View Details</th>
+            </tr>
+        </thead>
+        <tbody>
+          {<ListProperty gridOrList={this.state.gridOrList}/>}  
+        </tbody> 
+    </table>
+    </Col>
+  }
+
+  render(){
+    return (
+      <Container className="App">
+        <Row className="property-row">   
+          <Col md={{span:4, offset:8}} className="btn-block">
+            <button className="btn btn-secondary" onClick={() => this.handleClick('list')}>List View</button>
+            <button className="btn btn-primary" onClick={() => this.handleClick('grid')}>Grid View</button>
+          </Col>
+        {this.ListorGridViewByFlag()}
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ViewProperty from './components/ViewProperty';
+import store from './server/store';
 import * as serviceWorker from './serviceWorker';
+import { Route, Router } from "react-router-dom";
+import { createBrowserHistory as createHistory } from 'history';
+import { fetchAllProperties } from './actions'
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory();
+store.dispatch(fetchAllProperties());
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route exact path="/" component={App} />
+            <Route exact path="/:id" component={ViewProperty} />
+        </Router>
+    </Provider>,
+
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
